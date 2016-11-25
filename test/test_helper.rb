d50d6469b7048d2024d3d2cdd5de4fce7ccc5b16
aki017 <aki@aki017.info>
2016-11-25 16:32:29 +0900

    change for ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,5 @@
 require 'simplecov'
 
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
-end
-
 SimpleCov.start do
   add_filter '/test/'
 end
@@ -13,12 +8,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'zero_formatter'
 
 require "minitest/reporters"
-if ENV["CIRCLE_TEST_REPORTS"]
-  report_path = File.join(ENV["CIRCLE_TEST_REPORTS"], "test")
-else
-  report_path = "test/reports"
-end
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new, Minitest::Reporters::JUnitReporter.new(report_path)]
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new, Minitest::Reporters::JUnitReporter.new]
 require 'minitest/autorun'
 require 'minitest/pride'
 
